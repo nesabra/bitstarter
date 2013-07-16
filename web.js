2ab68f170e5b937e2262487f113632345d2f8e72
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-16 18:44:00 +0000

    web.js works with console.log

--- web.js
+++ web.js
@@ -9,8 +9,8 @@ var fs = require('fs');
 
 var indexFile = fs.readFileSync('index.html');
 
-console.log(indexFile.toString('utf-8', 0, 27));
-//process.stdout.write(indexFile.toString("utf-8", 0,27));
+//console.log(indexFile.toString('utf-8', 0, 27));
+process.stdout.write(indexFile.toString('utf-8', 0,27));
 
 /*var port = process.env.PORT || 5000;
 app.listen(port, function() {
