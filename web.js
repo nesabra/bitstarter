fc1ccb31448aa3baff877d950a22348ff8667c43
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-16 19:10:14 +0000

    committing web.js

--- web.js
+++ web.js
@@ -10,10 +10,10 @@ app.get('/', function(request, response) {
   response.send('yeah !');
 });
 
-console.log(indexFile.toString('utf-8', 0, 27));
+//console.log(indexFile.toString('utf-8', 0, 27));
 //process.stdout.write(indexFile.toString('utf-8', 0,27));
 
-/*var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});*/
+});
