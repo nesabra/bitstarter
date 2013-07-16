762663a985d2fa14e557d7a5e389464536b4d8ec
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-16 18:25:05 +0000

    web.js corriged

--- web.js
+++ web.js
@@ -9,7 +9,8 @@ var fs = require('fs');
 
 var indexFile = fs.readFileSync("index.html");
 
-console.log(indexFile.toString("utf-8", 0, 279));
+//console.log(indexFile.toString("utf-8", 0, 279));
+process.stdout.write(indexFile.toString("utf-8", 0,27));
 
 /*var port = process.env.PORT || 5000;
 app.listen(port, function() {
