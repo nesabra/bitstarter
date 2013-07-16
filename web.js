19240b29c0e4b8dd6123bc8c74dede077d64b0cb
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-16 18:47:30 +0000

    web.js works with console.log

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-//var express = require('express');
+var express = require('express');
 var fs = require('fs');
 
-//var app = express.createServer(express.logger());
-
-/*app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});*/
+var app = express.createServer(express.logger());
 
 var indexFile = fs.readFileSync('index.html');
 
+app.get('/', function(request, response) {
+  response.send(indexFile.toString('utf-8', 0,27));
+});
+
 //console.log(indexFile.toString('utf-8', 0, 27));
-process.stdout.write(indexFile.toString('utf-8', 0,27));
+//process.stdout.write(indexFile.toString('utf-8', 0,27));
 
 /*var port = process.env.PORT || 5000;
 app.listen(port, function() {
