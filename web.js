c70636f53e5f1acb4565a9768dcc122333ea12da
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-16 18:20:27 +0000

    web.js modified and correctly running

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
-var express = require('express');
+//var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
 /*app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });*/
 
-var indexFile = fs.readFileSync(index.html);
+var indexFile = fs.readFileSync("index.html");
 
-indexFile.toString("utf-8", 0, 27);
+console.log(indexFile.toString("utf-8", 0, 279));
 
 /*var port = process.env.PORT || 5000;
 app.listen(port, function() {
