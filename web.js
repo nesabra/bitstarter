73a267ff6d60c984cae8b253b50a077380c79633
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-18 13:56:34 +0000

    web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+ 
 var indexFile = fs.readFileSync('index.html');
 
 //app.set('views', __dirname + '/views');
