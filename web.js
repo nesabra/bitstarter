0869297a1c29d3b2a64f871078800384e55abd15
Ubuntu <ubuntu@ip-172-31-47-86.us-west-2.compute.internal>
2013-07-18 14:25:44 +0000

    web.js render

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
  
 var indexFile = fs.readFileSync("index.html");
 
-//app.set('views', __dirname + '/views');
-//app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
 //  response.send(indexFile.toString('utf-8', 0,fs.length));
