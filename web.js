c1d2c1fecef201103c7ed6eca6e907b74abc772a
Ubuntu <ubuntu@ip-172-31-29-56.us-west-2.compute.internal>
2013-07-22 17:17:50 +0000

    Creating staging heroku app

--- web.js
+++ web.js
@@ -22,7 +22,7 @@ app.get('/', function(request, response) {
 //console.log(indexFile.toString('utf-8', 0, 27));
 //process.stdout.write(indexFile.toString('utf-8', 0,27));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
